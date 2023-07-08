24f3caf3c497c0407a19c2fff592c2d87872dd5e
internetdrew <andrew@internetdrew.com>
2023-07-08 17:18:52 -0400

    add new page file to repo

---
+++ src/pages/sell-an-item.tsx
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  description: string;
+  price: number;
+};
+
+const SellAnItem = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+
+  return (
+    <>
+      <Head>
+        <title>Sell an Item</title>
+        <meta name="description" content="Sell an item on the T3 marketplace" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-800">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1>Sell an item</h1>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex w-1/3 flex-col gap-12"
+          >
+            <div>
+              <label
+                htmlFor="name"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Product Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Product name"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="description"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <input
+                type="text"
+                id="description"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Product description"
+                required
+              />
+            </div>
+            <div className="mx-auto">
+              <label
+                htmlFor="price"
+                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Price
+              </label>
+              <input
+                type="number"
+                step="0.01"
+                id="price"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                placeholder="Product price"
+                required
+              />
+            </div>
+
+            <input
+              type="submit"
+              className="cursor-pointer rounded-md bg-gray-600 py-2 font-bold duration-200 hover:bg-gray-500 hover:shadow-xl"
+            />
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default SellAnItem;
