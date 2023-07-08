92c173f14ab6547ca8a809876c3e62a73891e8e3
internetdrew <andrew@internetdrew.com>
2023-07-08 15:54:00 -0400

    add signin button and modal to navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { Navbar } from "~/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Navbar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
